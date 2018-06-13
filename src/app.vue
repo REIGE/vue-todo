e3db2088f4f907c5a7507162929843214b3098ab
reige <407601459@qq.com>
2018-06-13 23:04:09 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <div id="cover"></div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from './todo/header.vue'
+import Footer from './todo/footer.jsx'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+<style>
+#test {
+  color: red;
+}
+</style>
